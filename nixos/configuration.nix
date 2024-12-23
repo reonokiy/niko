a6a8abb10c93ae143df81fd9a7d817ca17c7660b
reonokiy <reonokiy@gmail.com>
2024-12-23 12:33:40 +0800

    fs: add ntfs support

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -8,6 +8,7 @@
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.supportedFilesystems = [ "ntfs" ];
 
   nix.settings.experimental-features = [
     "nix-command"

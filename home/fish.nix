dfb56ee1064df0060f4e85d55ffbd7e478c51790
reonokiy <reonokiy@gmail.com>
2024-12-24 20:02:33 +0800

    nix: add github token to avoid rate limit

--- home/fish.nix
+++ home/fish.nix
@@ -14,5 +14,23 @@
     shellInit = ''
       set fish_greeting
     '';
+    functions = {
+      load_nix_secrets = {
+        body = ''
+          if not command -v op > /dev/null
+            echo "1Password CLI is not installed. Please install it first."
+            return 1
+          end
+
+          set -Ux NIX_CONFIG "access-tokens = github.com="$(op read op://default-secrets/github_api_token_nix/token)
+        '';
+      };
+
+      unload_nix_secrets = {
+        body = ''
+          set -e NIX_CONFIG
+        '';
+      };
+    };
   };
 }
